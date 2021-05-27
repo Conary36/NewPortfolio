96b64c16c4c4bc9b455ca57fd8add7689ba882ce
Conary36 <crossingwaters@icloud.com>
2021-05-26 23:20:44 -0400

    update projects.scss styling

--- my_folio/src/components/Projects.js
+++ my_folio/src/components/Projects.js
@@ -5,10 +5,9 @@ import "swiper/components/pagination/pagination.min.css";
 import SwiperCore, { Mousewheel, Pagination } from "swiper/core";
 import firebase from "../firebase";
 import { Card, Image, Button, ButtonGroup } from "react-bootstrap";
-import '../styles/projects.scss'
+import "../styles/projects.scss";
+// import "../styles/swiper.css";
 // import {Link} from 'react-router-dom'
-
-
 
 export default function Projects() {
   const [projectData, setProjectData] = useState([]);
@@ -22,9 +21,7 @@ export default function Projects() {
     setLoading(false);
   };
 
-
   SwiperCore.use([Mousewheel, Pagination]);
-
 
   // const swiper = (".projects-slider", {
   //   spaceBetween: 30,
@@ -52,59 +49,108 @@ export default function Projects() {
 
   console.log("projectData==>>", projectData);
 
+  // description: "some derjhfdkfbdjhbkjlsdbjhlbjsbhjbsdhjbdsjbh";
+  // projectImg: "stash.png";
+  // projectName: "Stash";
+  // projectUrl: "https://stashfoodapp.netlify.app";
+
   return (
     <Swiper
       direction={"vertical"}
-      // slidesPerView={1}
-      effect={"fade"}
-      loop={true}
+      slidesPerView={1}
       spaceBetween={30}
-      mousewheel={{invert: false}}
+      mousewheel={true}
       pagination={{
-        el: "projects-slider__pagination",
         clickable: true,
       }}
-     
+      className="mySwiper"
     >
-      <div className="projects-slider swiper-container-fade swiper-container-horizontal">
-      <div className="projects-slider__wrp swiper-wrapper">
-        <div className="projects-slider__item swiper-slide swiper-slide-duplicate swiper-slide-duplicate-active">
-          {loading ? <h1>Loading...</h1> : null}
-          {projectData.map((proj, i) => (
-            <SwiperSlide>
-              <Card key={i} className="">
-                {proj.projectImg && (
-                  <span className="projects-slider__img">
-                    <Image
-                      src={require(`../images/${proj.projectImg}`).default}
-                      alt={proj.alt}
-                      fluid
-                    />
-                    
-                  </span>
-                )}
-                <div className="projects-slider__content">
-                  <div className="projects-slider__title">
-                    {proj.projectName}
-                  </div>
-                  <div className="projects-slider__text">
-                    {proj.description}
-                  </div>
-                </div>
+      {projectData.map((proj, i) => (
+        <Card key={i}>
+          <SwiperSlide>
+            {proj.projectImg && (
+              <Image
+                className="projectImg"
+                src={require(`../images/${proj.projectImg}`).default}
+                alt={proj.alt}
+                fluid
+              />
+            )}
 
-                <a
-                  href={proj.projectUrl}
-                  className="project-slider__button"
-                  target="_blank"
-                >
-                  Open Link
-                </a>
-              </Card>
-            </SwiperSlide>
-          ))}
-        </div>
-      </div>
-      </div>
+            <div className="secondPart">
+              <ul>
+                <li>{proj.projectName}</li>
+                <li>{proj.description}</li>
+                <li>
+                  {" "}
+                  <Button
+                    href={proj.projectUrl}
+                    className="projects-slider__button"
+                    target="_blank"
+                  >
+                    Open Link
+                  </Button>
+                </li>
+              </ul>
+            </div>
+          </SwiperSlide>
+        </Card>
+      ))}
     </Swiper>
+
+    // <Swiper
+    //   className="projects-slider"
+    //   spaceBetween={30}
+    //   direction={"vertical"}
+    //   slidesPerView={1}
+    //   effect={"fade"}
+    //   loop={true}
+    //   mousewheel={{ invert: false }}
+    //   pagination={{
+    //     el: "projects-slider__pagination",
+    //     clickable: true,
+    //   }}
+    // >
+    //   <div className="projects-slider swiper-container-fade swiper-container-horizontal">
+    //     <div className="projects-slider__wrp swiper-wrapper">
+    //       <div className="projects-slider__item swiper-slide swiper-slide-duplicate swiper-slide-duplicate-active">
+    //         {loading ? <h1>Loading...</h1> : null}
+
+    //         {projectData.map((proj, i) => (
+    //           <Card key={i} className="">
+    //             <SwiperSlide>
+    //               {proj.projectImg && (
+    //                 <span className="projects-slider__img">
+    //                   <Image
+    //                     src={require(`../images/${proj.projectImg}`).default}
+    //                     alt={proj.alt}
+    //                     fluid
+    //                   />
+    //                 </span>
+    //               )}
+    //               <div className="projects-slider__content">
+    //                 <div className="projects-slider__title">
+    //                   {proj.projectName}
+    //                 </div>
+    //                 <div className="projects-slider__text">
+    //                   {proj.description}
+    //                 </div>
+    //               </div>
+
+    //               <Button
+    //                 href={proj.projectUrl}
+    //                 className="projects-slider__button"
+    //                 target="_blank"
+    //               >
+    //                 Open Link
+    //               </Button>
+    //             </SwiperSlide>
+    //           </Card>
+    //         ))}
+    //       </div>
+    //     </div>
+    //     <div className="projects-slider__pagination"></div>
+    //   </div>
+    // </Swiper>
   );
 }
