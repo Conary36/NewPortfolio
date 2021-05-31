9324c53c84473b821d1c680255ff12252e0a036d
Conary36 <crossingwaters@icloud.com>
2021-05-31 14:22:49 -0400

    working model...projects.scss

--- my_folio/src/components/Projects.js
+++ my_folio/src/components/Projects.js
@@ -2,12 +2,16 @@ import React, { useState, useEffect } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/swiper.min.css";
 import "swiper/components/pagination/pagination.min.css";
-import SwiperCore, { Mousewheel, Pagination } from "swiper/core";
+import SwiperCore, { Mousewheel, Pagination, EffectFade, Navigation } from "swiper/core";
 import firebase from "../firebase";
 import { Card, Image, Button, ButtonGroup } from "react-bootstrap";
 import "../styles/projects.scss";
+// import { EffectFade } from "swiper/js/swiper.esm";
 // import "../styles/swiper.css";
 // import {Link} from 'react-router-dom'
+import "swiper/components/effect-fade/effect-fade.min.css";
+import "swiper/components/navigation/navigation.min.css";
+import "swiper/components/pagination/pagination.min.css";
 
 export default function Projects() {
   const [projectData, setProjectData] = useState([]);
@@ -21,21 +25,9 @@ export default function Projects() {
     setLoading(false);
   };
 
-  SwiperCore.use([Mousewheel, Pagination]);
+  SwiperCore.use([Mousewheel, Pagination, EffectFade, Navigation ]);
 
-  // const swiper = (".projects-slider", {
-  //   spaceBetween: 30,
-  //   effect: "fade",
-  //   loop: true,
-  //   mousewheel: {
-  //     invert: false,
-  //   },
-  //   // autoHeight: true,
-  //   pagination: {
-  //     el: ".projects-slider__pagination",
-  //     clickable: true,
-  //   },
-  // });
+  
 
   // eslint-disable-next-line react-hooks/rules-of-hooks
   useEffect(() => {
@@ -59,106 +51,47 @@ export default function Projects() {
       <Swiper
         className="projects-slider"
         direction={"vertical"}
-        // slidesPerView={1}
+        slidesPerView={1}
         spaceBetween={30}
         effect={"fade"}
         loop={true}
-        mousewheel={{ invert: false }}
+        mousewheel={true}
         pagination={{
           el: "projects-slider__pagination",
           clickable: true,
         }}
-        // className="mySwiper"
       >
-       
-          {projectData.map((proj, i) => (
-            <SwiperSlide
-              className="parentSwiper"  
-            >
-              {proj.projectImg && (
-                <div className="projects-slider__img">
-                  <img
-                    src={require(`../images/${proj.projectImg}`).default}
-                    alt={proj.alt}
-                    fluid
-                  />
-                </div>
-              )}
-
-              <div className="projects-slider__content">
-                <div className="projects-slider__title">{proj.projectName}</div>
-                <div className="projects-slider__text">{proj.description}</div>
+        {projectData.map((proj, i) => (
+          <SwiperSlide className="parentSwiper" key={i}>
+            {proj.projectImg && (
+              <div className="projects-slider__img">
+                <img
+                  src={require(`../images/${proj.projectImg}`).default}
+                  alt={proj.alt}
+                  fluid
+                />
               </div>
+            )}
 
-              <Button
-                href={proj.projectUrl}
-                className="projects-slider__button"
-                target="_blank"
-              >
-                Open Link
-              </Button>
-            </SwiperSlide>
-          ))}
+            <div className="">
+              <div className="">{proj.projectName}</div>
+              <div className="">{proj.description}</div>
+            </div>
 
-          <div className="projects-slider__pagination"></div>
-       
+            <Button
+              href={proj.projectUrl}
+              className="projects-slider__button"
+              target="_blank"
+            >
+              Open Link
+            </Button>
+          </SwiperSlide>
+        ))}
       </Swiper>
+      <div className="projects-slider__pagination"></div>
     </div>
 
-    // <Swiper
-    //   className="projects-slider"
-    //   spaceBetween={30}
-    //   direction={"vertical"}
-    //   slidesPerView={1}
-    //   effect={"fade"}
-    //   loop={true}
-    //   mousewheel={{ invert: false }}
-    //   pagination={{
-    //     el: "projects-slider__pagination",
-    //     clickable: true,
-    //   }}
-    // >
-    //   <div className="projects-slider swiper-container-fade swiper-container-horizontal">
-    //     <div className="projects-slider__wrp swiper-wrapper">
-    //       <div className="projects-slider__item swiper-slide swiper-slide-duplicate swiper-slide-duplicate-active">
-    //         {loading ? <h1>Loading...</h1> : null}
-
-    //         {projectData.map((proj, i) => (
-    //           <Card key={i} className="">
-    //             <SwiperSlide>
-    //               {proj.projectImg && (
-    //                 <span className="projects-slider__img">
-    //                   <Image
-    //                     src={require(`../images/${proj.projectImg}`).default}
-    //                     alt={proj.alt}
-    //                     fluid
-    //                   />
-    //                 </span>
-    //               )}
-    //               <div className="projects-slider__content">
-    //                 <div className="projects-slider__title">
-    //                   {proj.projectName}
-    //                 </div>
-    //                 <div className="projects-slider__text">
-    //                   {proj.description}
-    //                 </div>
-    //               </div>
-
-    //               <Button
-    //                 href={proj.projectUrl}
-    //                 className="projects-slider__button"
-    //                 target="_blank"
-    //               >
-    //                 Open Link
-    //               </Button>
-    //             </SwiperSlide>
-    //           </Card>
-    //         ))}
-    //       </div>
-    //     </div>
-    //     <div className="projects-slider__pagination"></div>
-    //   </div>
-    // </Swiper>
+   
   );
 }
 
