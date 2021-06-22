b8aae7c0c528aabe6a7b84f6f348aa3ecc578bce
Conary36 <crossingwaters@icloud.com>
2021-06-22 01:09:51 -0400

    npm Tilt added...

--- my_folio/src/components/Home.js
+++ my_folio/src/components/Home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import '../styles/home.scss'
 import Image from 'react-bootstrap/Image'
+import Tilt from "react-parallax-tilt";
 
 export default function Home(){
     return (
@@ -8,12 +9,14 @@ export default function Home(){
         <section className='home_img'>
              <Image fluid/>
         </section>
-        <section className='bio'>
-        <span>Hello, my name is Conary,
-            I am a Front-End Web Developer with knowledge of<br/>
-            the Back-End. What can we create together?
-        </span><br/>
-        </section>
+        <Tilt>
+            <section className='bio'>
+            <span>Hello, my name is Conary,
+                I am a Front-End Web Developer with knowledge of<br/>
+                the Back-End. What can we create together?
+            </span><br/>
+            </section>
+        </Tilt>
      </div>
         
         
