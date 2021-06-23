c5a118c12d49783d88b04cbc87eee04401b5361f
Conary36 <crossingwaters@icloud.com>
2021-06-23 02:47:27 -0400

    updated card css

--- my_folio/src/components/About.js
+++ my_folio/src/components/About.js
@@ -1,21 +1,26 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
 import '../styles/about.scss'
+import Tilt from "react-parallax-tilt";
 
 export default function About(){
 
 return (
-    <>
-    <div id='title' className='transitions'>
-         <h1>About</h1>
-         <span>Programming a new world one project at a time, this is my passion.</span><br/> 
-         <span>My creative abilities come to life</span>
 
-        <Link id='p_btn' to='/projects'>
-            <button>Projects</button>
-        </Link>
+    <div id='title' className='transitions'>
+        <Tilt>
+            <section className="about_bio">
+                <h1>About</h1>
+                <span>Programming a new world one project at a time, this is my passion.</span><br/> 
+                <span>My creative abilities come to life</span>
+
+                <Link id='p_btn' to='/projects'>
+                    <button>Projects</button>
+                </Link>
+            </section>
+        </Tilt>
     </div>
-    </>
+
         
  )
 }
